a9647dbe04e0efe14cc88aa2aa13770b55b45a32
Lev Dvorniy <levdvorniy@gmail.com>
2022-09-09 12:24:51 +0600

    Тесты для точки и линии, часть тестов для полигонов и функций вычисления расстояний

---
+++ GeometryModels.Tests/Objects/PointTests.cs
@@ -0,0 +1,37 @@
+﻿public class PointTests
+{
+    [Fact]
+    public void CreatePoint_Success()
+    {
+        //Arrage.
+        public Point point1 = new Point(1, 3);
+        //Act.
+        double y = point1.GetY();
+        //Assert.
+        Assert.Equal(y, 3);
+    }
+
+    [Fact]
+    public void AssignPoint_Success()
+    {
+        //Arrage.
+        public Point point1 = new Point(1, 3);
+        public Point point2 = point1;
+        //Act.
+        double y = point2.GetY();
+        //Assert.
+        Assert.Equal(y, 3);
+    }
+    
+
+    [Fact]
+    public void PointsDimensions_Succes()
+    {
+        //Arrage.
+        public Point point1 = new Point(1, 3);
+        //Act.
+        double dimension = point1.GetDimension();
+        //Assert.
+        Assert.Equal(dimension, 2);
+    }
+}
