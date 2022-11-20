9e6a471bae68145f3a21179be31de78caea93272
Levdvorniy <levdvorniy@gmail.com>
2022-11-21 01:40:31 +0600

    Доделан метод возврата линии наименьшего расстояния между объектами

---
+++ GeometryModels.Tests/ShortestLineSearcher/LineShortestSearcherTests.cs
@@ -0,0 +1,25 @@
+﻿using GeometryModels.Models;
+using GeometryModels.Visitors.ShortestLineSearchers.ModelsShortestLineSearcher;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeometryModels.Tests.ShortestLineSearcher
+{
+	public class LineShortestSearcherTests
+	{
+		[Fact]
+		public static void GetShortestLineBetweenLineAndPoint()
+		{
+			//Arrage.
+			Line line = new Line(new Point(0,0), new Point(3,3));
+			Point point = new Point(0,2);
+			//Act.
+			Line shortLine = LineShortestLineSearcher.GetShortestLine(line, point);
+			//Assert.
+			Assert.Equal(2, shortLine.GetLength());
+		}
+	}
+}
