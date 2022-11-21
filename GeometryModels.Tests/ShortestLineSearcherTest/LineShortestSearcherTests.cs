9f89468fb3d67797616aee7b11021db459c128e9
Levdvorniy <levdvorniy@gmail.com>
2022-11-21 15:12:09 +0600

    Исправил названия переменных и пространств имён

--- GeometryModels.Tests/ShortestLineSearcherTest/LineShortestSearcherTests.cs
+++ GeometryModels.Tests/ShortestLineSearcherTest/LineShortestSearcherTests.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace GeometryModels.Tests.ShortestLineSearcher
+namespace GeometryModels.Tests.ShortestLineSearcherTest
 {
 	public class LineShortestSearcherTests
 	{
