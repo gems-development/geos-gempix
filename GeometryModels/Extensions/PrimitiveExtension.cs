440a79f7be90e8797340dd78052dcc3122476de5
Anastasia Kravtsova <anastasya.kallisto@gmail.com>
2022-11-27 17:14:02 +0600

    Собрала все расширения в один класс PrimitiveExtension

---
+++ GeometryModels/Extensions/PrimitiveExtension.cs
@@ -0,0 +1,26 @@
+﻿using GeometryModels.Models;
+using GeometryModels.Visitors.DistanceCalculators;
+using GeometryModels.Visitors.Insiders;
+using GeometryModels.Visitors.Intersectors;
+using GeometryModels.Visitors.ShortestLineSearchers;
+
+namespace GeometryModels.Extensions
+{
+    public class PrimitiveExtension
+    {
+        public static double GetDistance(IGeometryPrimitive primitive1, IGeometryPrimitive primitive2) =>
+            new DistanceCalculator(primitive1, primitive2).GetResult();
+
+        public static bool IsInside(IGeometryPrimitive primitive1, IGeometryPrimitive primitive2) =>
+                new Insider(primitive1, primitive2).GetResult();
+
+        public static bool Intersects(IGeometryPrimitive primitive1, IGeometryPrimitive primitive2) =>
+                new Intersector(primitive1, primitive2).GetResult();
+
+        public static Line GetShortestLine(IGeometryPrimitive primitive1, IGeometryPrimitive primitive2) =>
+                new ShortestLineSearcher(primitive1, primitive2).GetResult();
+
+        public static bool IsTouching(IGeometryPrimitive primitive1, IGeometryPrimitive primitive2) =>
+                new Toucher(primitive1, primitive2).GetResult();
+    }
+}
--- GeometryModels/Extensions/ToucherExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GeometryModels.Extensions
-{
-    public static class ToucherExtension
-    {
-        public static bool IsTouching(this IGeometryPrimitive primitive1, IGeometryPrimitive primitive2) =>
-            new Toucher(primitive1, primitive2).GetResult();
-    }
-}
