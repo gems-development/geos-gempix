ad92b39dc778a1c44ff17093c5946f1a20e529f6
Anastasia Kravtsova <anastasya.kallisto@gmail.com>
2022-12-08 22:33:34 +0600

    Поправки из-за пакета Extension

--- GeosGempix/Extensions/ToucherExtension.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GeosGempix.Extensions
-{
-    public class ToucherExtension
-    {
-        public static bool IsTouching(IGeometryPrimitive primitive1, IGeometryPrimitive primitive2) =>
-            new Toucher(primitive1, primitive2).GetResult();
-    }
-}
