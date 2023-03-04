84d0bd2c495772f39df02619346187b9290d7588
Vlad Ilyashenko <vlad180317@gmail.com>
2023-03-04 14:04:55 +0600

    *часть тестов на LineDistanceCalculator

--- GeosGempix.Tests/DistanceTest/PolygonDistanceCalculatorTests.cs
+++ GeosGempix.Tests/DistanceTest/PolygonDistanceCalculatorTests.cs
@@ -9,30 +9,24 @@ namespace GeosGempix.Tests.DistanceTest
 {
     public class PolygonDistanceCalculatorTests
     {
-		// Проверка на растояние между полигоном и точкой
+		// Проверка на растояние между полигоном и контуром
 		[Fact]
-        public void GetDistanceBetweenPolygonAndPoint_Success()
-        {
-            //Arrange.
-            var point1 = new Point(0, 0);
-            var point2 = new Point(0, 3);
-            var point3 = new Point(5, 4);
-            var point4 = new Point(3, 2);
-            var point5 = new Point(0, 0);
-            var points = new List<Point>
-            {
-                point1,
-                point2,
-                point3,
-                point4,
-                point5
-            };
-            var polygon = new Polygon(points);
-            
-            var point = new Point(2, 2);
-            
-            //Act. + Assert.
-            Assert.Equal(0, polygon.GetDistance(point));
-        }
+        public void GetDistanceBetweenPolygonAnd_Success() { }
+        
+        // Проверка на растояние между полигоном и полигоном
+        [Fact]
+        public void GetDistanceBetweenPolygonAndPolygon_Success() { }
+        
+        // Проверка на растояние между полигоном и мультиточкой
+        [Fact]
+        public void GetDistanceBetweenPolygonAndMultiPoint_Success() { }
+        
+        // Проверка на растояние между полигоном и мультилинией
+        [Fact]
+        public void GetDistanceBetweenPolygonAndMultiLine_Success() { }
+        
+        // Проверка на растояние между полигоном и мультиполигоном
+        [Fact]
+        public void GetDistanceBetweenPolygonAndMultiPolygon_Success() { }
     }
 }
